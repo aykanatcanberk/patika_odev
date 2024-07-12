3242c3e108e0761607ab0df61a7325c3fe65aa7d
Canberk Aykanat <101566413+aykanatcanberk@users.noreply.github.com>
2024-07-12 14:06:11 +0300

    odev1.sql

--- odev1.sql
+++ odev1.sql
@@ -2,17 +2,14 @@ SELECT title, description FROM film;
 
 
 SELECT * FROM film
-WHERE length > 60
-  AND length < 75;
+WHERE length > 60 AND length < 75;
 
 
 SELECT * FROM film
-WHERE rental_rate = 0.99 AND replacement_cost = 12.99
-   OR replacement_cost = 28.99;
+WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
 
 
-SELECT last_name
-FROM customer
+SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
 SELECT * FROM film

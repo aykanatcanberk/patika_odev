c407119a4d9f4bb0702f458fa83b7299f52fde74
Canberk Aykanat <101566413+aykanatcanberk@users.noreply.github.com>
2024-07-12 10:05:54 +0300

    odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,19 @@
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+
+
+SELECT country FROM country
+WHERE LENGTH(country) >= 6 AND country LIKE '%n';
+
+
+
+SELECT title
+FROM film
+WHERE 
+    LENGTH(title) - LENGTH(REPLACE(LOWER(title), 't', '')) +
+    LENGTH(title) - LENGTH(REPLACE(LOWER(title), 'T', '')) >= 4;
+
+
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;

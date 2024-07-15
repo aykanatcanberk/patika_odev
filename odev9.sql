bc5ad7ded9e71ad5f0cd1901d46ad32cd0b7bb2c
Canberk Aykanat <101566413+aykanatcanberk@users.noreply.github.com>
2024-07-15 18:09:30 +0300

    odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,12 @@
+SELECT city.city,country.country FROM city
+JOIN country ON city.country_id=country.country_id;
+
+
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id;
+
+
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+JOIN customer ON customer.customer_id=rental.customer_id;
